0789254b3b08d54c140f4076bd62f7e3ce3a1dcf
Fabio Fumarola <fumarolaf@prometeia.com>
2020-07-27 19:35:32 +0200

    update

--- setup.py
+++ setup.py
@@ -4,13 +4,17 @@ from distutils.core import setup
 setup(
     name='cookiecutter-pypackage',
     packages=[],
-    version='1.0.0',
+    version='0.1.0',
     description='Cookiecutter template for a Python package',
     author='Audrey Roy Greenfeld',
     license='BSD',
     author_email='aroy@alum.mit.edu',
     url='https://github.com/audreyr/cookiecutter-pypackage',
-    keywords=['cookiecutter', 'template', 'package', ],
+    keywords=[
+        'cookiecutter',
+        'template',
+        'package',
+    ],
     python_requires='>=3.5',
     classifiers=[
         'Development Status :: 4 - Beta',
